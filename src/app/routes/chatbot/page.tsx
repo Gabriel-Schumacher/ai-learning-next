2970876fa56b35bdd893e98d50ce9e2676e75c76
Gabriel-Schumacher <schumachergabe@gmail.com>
2025-05-29 17:18:01 -0600

    Started data creation page

--- src/app/routes/chatbot/page.tsx
+++ src/app/routes/chatbot/page.tsx
@@ -6,6 +6,8 @@ import FigmaNavigation from "../../components/FigmaNavigation"
 import { useEffect, useContext } from "react"
 import { AiDataProviderContext } from "../../components/AiContextProvider/AiDataProvider"
 import AiQuiz from "../../components/AiQuiz/AiQuiz"
+import DataCreation from "../datacreation/page"
+
 import QuizContextProvider from "../../components/AiQuiz/QuizContext"
 const BASE_URL = process.env.BASE_URL || 'http://localhost:3000';
 
@@ -72,11 +74,7 @@ function ChatBot() {
                         }
                         {/* Data Creation */}
                         {data.currentPage === "DATA_CREATION" &&
-                            <div className="w-full h-full grid place-items-center">
-                                <div>
-                                    <p className="bg-error-500 rounded py-1 px-2 text-surface-950-50 font-bold mb-2">Not set up yet 😞</p>
-                                </div>
-                            </div>
+                                <DataCreation />
                         }
                     </>
                 }
